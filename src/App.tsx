f0963db124051d22313073639ff9c6148692708b
devoil <devdevoildev@gmail.com>
2025-04-07 14:12:24 +0900

    Feat: [#3] ResultPage 구현

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,3 @@
-import { useState } from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { HomePage } from './pages/HomePage';
 import { AboutPage } from './pages/AboutPage';
