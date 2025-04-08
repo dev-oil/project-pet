3280b6fafd564c08a79aa4e30ab7cb7b70393e73
devoil <devdevoildev@gmail.com>
2025-04-08 10:52:13 +0900

    Feat: [#3] HomePage 메인 구현

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -3,7 +3,7 @@ import { Header } from './Header';
 
 export const Layout = ({ children }: { children: ReactNode }) => {
   return (
-    <div className='font-sans text-black bg-white'>
+    <div className='text-black bg-white'>
       <Header />
       {children}
     </div>
