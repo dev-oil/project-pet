b8ceed61259383bb0d01fe7a1976297fbad8699c
devoil <devdevoildev@gmail.com>
2025-04-13 21:09:56 +0900

    Fix: [#13] import 수정

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
+import { Layout } from './components/Layout';
 import { AboutPage } from './pages/AboutPage';
 import { AnimalsDetailPage } from './pages/AnimalsDetailPage';
 import { AnimalsPage } from './pages/AnimalsPage';
@@ -7,7 +8,6 @@ import { HomePage } from './pages/HomePage';
 import { ResultPage } from './pages/ResultPage';
 import { TestPage } from './pages/TestPage';
 import './App.css';
-import { Layout } from './components/Layout';
 
 function App() {
   return (
--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,4 +1,5 @@
 import { ReactNode } from 'react';
+
 import { Header } from './Header';
 
 export const Layout = ({ children }: { children: ReactNode }) => {
